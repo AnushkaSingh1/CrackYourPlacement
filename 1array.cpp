81d28b89f96f1b68255de269fe5c25f20ddb17db
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-13 20:22:13 +0530

    Update 1array.cpp

--- 1array.cpp
+++ 1array.cpp
@@ -18,3 +18,20 @@ public:
         return duplicate;
     }
 };
+
+
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map <int,int> dup;
+        for(int i=0;i<nums.size();i++){
+            dup[nums[i]]++;
+            if(dup[nums[i]]>1)
+                return nums[i];
+        }
+            return -1;
+
+    }
+};
