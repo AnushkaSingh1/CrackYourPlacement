4c481bdb22a6c03d8f9694f68c142c8042acf6d7
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-14 19:39:41 +0530

    Update 2.array.cpp

--- 2.array.cpp
+++ 2.array.cpp
@@ -20,3 +20,31 @@ public:
         }
     }
 };
+
+
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        unordered_map <int,int> map;
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(map[0]!=0)
+              {  nums[i]=0;
+               map[0]--;
+              }
+            
+            else if(map[1]!=0)
+               { nums[i]=1;
+                              map[1]--;
+}
+            else
+                {nums[i]=2;
+                               map[2]--;
+}
+        }
+    }
+};
