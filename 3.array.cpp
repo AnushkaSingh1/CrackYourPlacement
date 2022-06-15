ec1ad4fadf94e81889d255e17da1d8e813401827
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-15 22:00:01 +0530

    Update 3.array.cpp

--- 3.array.cpp
+++ 3.array.cpp
@@ -13,3 +13,24 @@ public:
         return ++count;
     }
 };
+
+
+
+
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for(int j=1;j<nums.size();j++)
+        {
+            if(nums[i] != nums[j])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+};
