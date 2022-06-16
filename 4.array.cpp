84a5b039e0864f3a2447994575d1984b940f3053
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-16 22:29:00 +0530

    Update 4.array.cpp

--- 4.array.cpp
+++ 4.array.cpp
@@ -77,3 +77,70 @@ public:
         }
     }
 };
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
+        bool rowFlag= false;
+        for(int i=0; i< matrix.size(); i++)
+        {
+            if(matrix[i][0]== 0)
+            {
+                rowFlag= true;
+            }
+        }
+        bool colFlag= false;
+        for(int j=0; j< matrix[0].size(); j++)
+        {
+            if(matrix[0][j]== 0)
+            {
+                colFlag= true;
+            }
+        }
+        for(int i=1; i< matrix.size(); i++)
+        {
+            for(int j=1; j< matrix[i].size(); j++)
+            {
+                if(matrix[i][j]== 0)
+                {
+                    matrix[0][j]= 0;
+                    matrix[i][0]= 0;
+                }
+            }
+        }
+        for(int i=1; i< matrix.size(); i++)
+        {
+            for(int j=1; j< matrix[i].size(); j++)
+            {
+                if(matrix[0][j]== 0 || matrix[i][0]== 0)
+                {
+                    matrix[i][j]= 0;
+                }
+            }
+        }
+        if(rowFlag)
+        {
+            for(int i=0; i< matrix.size(); i++)
+            {
+                matrix[i][0]= 0;
+            }
+        }
+        if(colFlag)
+        {
+            for(int j=0; j< matrix[0].size(); j++)
+            {
+                matrix[0][j]= 0;
+            }
+        }
+        return;
+    }
+};
