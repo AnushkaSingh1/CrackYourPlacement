b7081683d541620d414037e3844fd7dc3f2dbe56
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-18 11:17:15 +0530

    Update 5.array.cpp

--- 5.array.cpp
+++ 5.array.cpp
@@ -19,3 +19,29 @@ public:
         
     }
 };
+
+
+
+
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int i = 0;
+        int j = 1;
+        
+        while(i<arr.size() && j<arr.size()){
+            if(arr[i]==0 && arr[j]!=0){
+                swap(arr[i],arr[j]);
+                i++;
+                j++;
+            }else if(arr[i]==0 && arr[j]==0){
+                j++;
+            }else{
+                i++;
+                j++;
+            }
+        }
+    }
+};
