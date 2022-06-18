f54e8a0b96623cb4ce7651cf49f82e6ea43219b6
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-18 23:20:06 +0530

    Update 6.array.cpp

--- 6.array.cpp
+++ 6.array.cpp
@@ -16,3 +16,26 @@ public:
         return profit;
     }
 };
+
+
+
+
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty()) return 0;
+        int buyPrice = prices[0], maxProfit = 0, curProfit = 0;
+        
+        for(int i=1; i<prices.size(); i++){
+            if(prices[i] < buyPrice) buyPrice = prices[i];
+            else {
+				curProfit = prices[i] - buyPrice;
+				maxProfit = max(curProfit, maxProfit);
+			}
+        }
+        return maxProfit;
+    }
+};
