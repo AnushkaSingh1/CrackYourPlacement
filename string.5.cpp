216b4e795ef97c6a58d74b2d62e0472f21aa3e2f
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-23 15:48:38 +0530

    Update string.5.cpp

--- string.5.cpp
+++ string.5.cpp
@@ -34,3 +34,70 @@ public:
         
     }
 };
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool check(string &s, int b, int e){
+      while(b<e){
+        if(s[b] != s[e])
+          return false;
+        b++;e--;
+      }
+      return true;
+    }
+    bool validPalindrome(string s) {
+      int b = 0;
+      int e = s.length()-1;
+      bool deleted = false;
+      while(b<e){
+        if(s[b] == s[e]){
+          b++;e--;
+        }else{
+          return check(s, b+1, e) or check(s, b, e-1);
+        }
+      }
+      return true;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+ 
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+
+Input: s = "abc"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
