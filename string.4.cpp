c6fc3ba0d30860f9ffb2ee94502742aaf3e6bf68
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-23 15:45:11 +0530

    Update string.4.cpp

--- string.4.cpp
+++ string.4.cpp
@@ -17,3 +17,66 @@ string longestCommonPrefix(vector<string>& strs) {
        return output; 
 }
 };
+
+
+
+
+
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        sort(strs.begin(),strs.end());
+        string ans="";
+        string start=strs[0],end=strs[n-1];
+        for(int i=0;i<start.size();i++)
+        {
+            if(start[i]==end[i])
+            {
+                ans+=start[i];
+            }
+            else break;
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
