92ab7b7ac6ce6d260a46a918d615df0550604bfd
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-29 23:59:35 +0530

    Create ll1.cpp

---
+++ ll1.cpp
@@ -0,0 +1,82 @@
+ListNode* middleNode(ListNode* head) 
+  {
+		ListNode * f=head,*s=head;
+		while(f && f->next){
+        f=f->next->next;
+        s=s->next;
+    }
+    return s;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+  ListNode* middleNode(ListNode* head) {
+
+    ListNode* firstP= head;
+    ListNode* secondP = head;
+    
+    while((secondP != NULL) && secondP->next != NULL){    
+              firstP= firstP->next;
+             secondP= secondP->next->next;    
+    }
+    
+    return firstP;
+    
+}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
