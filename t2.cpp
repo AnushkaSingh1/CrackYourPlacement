cbc6800a42692653a28080c375358dc4c504b8b2
Anushka Singh <anushkasingh117733@gmail.com>
2022-07-07 12:15:34 +0530

    Update t2.cpp

--- t2.cpp
+++ t2.cpp
@@ -85,3 +85,83 @@ public:
         return root;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+        
+        TreeNode* lt= root->left;
+        TreeNode* rt= root->right;
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        root->left= rt;
+        root->right= lt;
+        
+        return root;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
