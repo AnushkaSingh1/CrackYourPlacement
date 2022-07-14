a34fca2217041324fad6d62700bed4fd156bdd04
Anushka Singh <anushkasingh117733@gmail.com>
2022-07-14 23:15:40 +0530

    Update t9.cpp

--- t9.cpp
+++ t9.cpp
@@ -68,3 +68,78 @@ public:
         return ans;  // return all paths
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        string output = "";
+        
+        helper(ans, output, root);
+        return ans;
+    }
+    void helper(vector<string>&ans, string output, TreeNode*root){
+        if(root->left == NULL and root->right==NULL)    //This is a leaf node
+        {
+			output += to_string(root->val);  
+			// to_string() function converts integer to string and if we do not use this function and directly push the root->val then all the elements of string will be appended by \u000 which will cause the answer to be incorrect
+            ans.push_back(output);
+            return;
+        }
+        output += to_string(root->val);
+        output += "->";
+        if(root->left) helper(ans, output, root->left);
+        if(root->right) helper(ans, output, root->right);
+        return;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+Example 2:
+
+Input: root = [1]
+Output: ["1"]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 100].
+-100 <= Node.val <= 100
