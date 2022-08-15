0f3a17744842cb6d436006265efa605070bb6b13
Anushka Singh <anushkasingh117733@gmail.com>
2022-08-15 23:37:53 +0530

    Create array.13.cpp

---
+++ array.13.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> res;
+        
+        for(int i=0;i<nums.size()-2;i++){
+            if(i==0 || i>0 && nums[i]!=nums[i-1]){
+                int lo=i+1,hi=nums.size()-1,sum = 0-nums[i];
+                while(lo<hi){
+                    if(nums[lo]+nums[hi]==sum){
+                        vector<int> temp;
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[lo]);
+                        temp.push_back(nums[hi]);
+                        res.push_back(temp);
+                        
+                        while(lo<hi && nums[lo]==nums[lo+1]) lo++;
+                        while(lo<hi && nums[hi]==nums[hi-1]) hi--;
+                        
+                        lo++;
+                        hi--;
+                    }
+                    else if(nums[lo]+nums[hi]<sum) lo++;
+                    else hi--;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+3Sum
+  
+  Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
