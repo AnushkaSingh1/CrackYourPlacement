7b2f2a8c7b36d80f151a43379142b319ff7e6d5d
Anushka Singh <anushkasingh117733@gmail.com>
2022-09-06 23:02:53 +0530

    Create ll13.cpp

---
+++ ll13.cpp
@@ -0,0 +1,94 @@
+class Solution {
+public:
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode *l3= new ListNode();
+    ListNode* head=l3;
+    int car=0;
+    while(l1!=NULL || l2!=NULL){
+        if(l1!=NULL){
+            l3->val=l3->val+l1->val;
+            l1=l1->next;
+        }
+        if(l2!=NULL){
+            l3->val=l3->val+l2->val;
+            l2=l2->next;
+        }
+        l3->val=(l3->val+car);
+        car=(l3->val)/10;
+        l3->val=(l3->val)%10;
+        if(l1!=NULL || l2!=NULL){
+            l3->next=new ListNode();
+            l3=l3->next;
+        }
+        
+    }
+    if(car!=0){
+        l3->next=new ListNode();
+        l3=l3->next;
+        l3->val=car;
+    }
+    
+    return head ;
+}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+2. Add Two Numbers
+Medium
+
+21230
+
+4178
+
+Add to List
+
+Share
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+
+
+
+
