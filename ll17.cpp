8794a5c4df8972ed29a0f240e32faa8fdd5f96fb
Anushka Singh <anushkasingh117733@gmail.com>
2022-09-10 23:54:33 +0530

    Create ll17.cpp

---
+++ ll17.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        stack<ListNode*> st;
+        ListNode* p{head}, *temp{head};
+        while (p != NULL) {
+            st.push(p);
+            p = p->next;
+        }
+        p = head;
+        while (p != st.top()) {
+            p = p->next;
+            temp->next = st.top();
+            if (st.top() == p) {
+                p->next = NULL;
+                break;
+            }
+            st.top()->next = p;
+            st.pop();
+            temp = p;
+        }
+        if (p == st.top()) p->next = NULL;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+143. Reorder List
+Medium
+
+7099
+
+244
+
+Add to List
+
+Share
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+Example 2:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5 * 104].
+1 <= Node.val <= 1000
