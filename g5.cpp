32f56f94ce32c8843d081711aae345dbf1239a94
Anushka Singh <anushkasingh117733@gmail.com>
2022-10-11 23:46:57 +0530

    Create g5.cpp

---
+++ g5.cpp
@@ -0,0 +1,96 @@
+class Solution {
+private:
+    void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src)
+    {
+        visited[src] = true;
+        for(int i : adj[src])
+            if(!visited[i])
+                dfs(adj, visited, i);
+    }
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size() < n - 1)
+            return -1;
+        vector<vector<int>> adj(n);
+        for(auto v : connections)
+        {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        vector<bool> visited(n, false);
+        int components = 0;
+        for(int i=0; i<n; i++)
+            if(!visited[i])
+            {
+                dfs(adj, visited, i);
+                components++;
+            }
+        return components - 1;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1319. Number of Operations to Make Network Connected
+Medium
+
+2751
+
+35
+
+Add to List
+
+Share
+There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
+
+You are given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected.
+
+Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
+
+ 
+
+Example 1:
+
+
+Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+Output: 1
+Explanation: Remove cable between computer 1 and 2 and place between computers 1 and 3.
+Example 2:
+
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+Output: 2
+Example 3:
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+Output: -1
+Explanation: There are not enough cables.
+ 
+
+Constraints:
+
+1 <= n <= 105
+1 <= connections.length <= min(n * (n - 1) / 2, 105)
+connections[i].length == 2
+0 <= ai, bi < n
+ai != bi
+There are no repeated connections.
+No two computers are connected by more than one cable
+
