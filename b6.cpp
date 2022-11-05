ae883acb16ec4062b2d22d3e35f6943cbe02e087
Anushka Singh <anushkasingh117733@gmail.com>
2022-11-05 23:55:05 +0530

    Create b6.cpp

---
+++ b6.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string> > ret;
+        if(s.empty()) return ret;
+        
+        vector<string> path;
+        dfs(0, s, path, ret);
+        
+        return ret;
+    }
+    
+    void dfs(int index, string& s, vector<string>& path, vector<vector<string> >& ret) {
+        if(index == s.size()) {
+            ret.push_back(path);
+            return;
+        }
+        for(int i = index; i < s.size(); ++i) {
+            if(isPalindrome(s, index, i)) {
+                path.push_back(s.substr(index, i - index + 1));
+                dfs(i+1, s, path, ret);
+                path.pop_back();
+            }
+        }
+    }
+    
+    bool isPalindrome(const string& s, int start, int end) {
+        while(start <= end) {
+            if(s[start++] != s[end--])
+                return false;
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+131. Palindrome Partitioning
+Medium
+
+8616
+
+268
+
+Add to List
+
+Share
+Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+
+ 
+
+Example 1:
+
+Input: s = "aab"
+Output: [["a","a","b"],["aa","b"]]
+Example 2:
+
+Input: s = "a"
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= s.length <= 16
+s contains only lowercase English letters.
+
+
+
+
+
+
+
+
+
